9d601252ef248e9566cacd8c7abcb7a452131d30
Graeme Tait <burriko@gmail.com>
2014-11-21 14:39:30 +0000

    Added braces to single-line ifs

--- Lib/FakerTestFixture.php
+++ Lib/FakerTestFixture.php
@@ -7,11 +7,13 @@ class FakerTestFixture extends CakeTestFixture
 	public function insert($db)
 	{
 		$generator = \Faker\Factory::create();
-		if ($seed = Configure::read('faker.seed'))
+		if ($seed = Configure::read('faker.seed')) {
 			$generator->seed($seed);
+		}
 
-		if ( ! ClassRegistry::isKeySet('faker'))
+		if ( ! ClassRegistry::isKeySet('faker')) {
 			ClassRegistry::addObject('faker', new Populator($generator));
+		}
 		$populator = ClassRegistry::getObject('faker');
 
 		$populator->addEntity($this->model_name, $this->num_records, $this->alterFields($generator));
